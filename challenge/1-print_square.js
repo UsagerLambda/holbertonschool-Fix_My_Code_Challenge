f381798675f8cd4270acf40c2fbdc0a07e2ffb7b
Utilisateur Lambda <clement.chassem@gmail.com>
2025-01-13 11:46:00 +0100

    Fix 1-print_square

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2])
+size = parseInt(process.argv[2], 10) // 10 as second argument to ensures the input is decimal number
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
